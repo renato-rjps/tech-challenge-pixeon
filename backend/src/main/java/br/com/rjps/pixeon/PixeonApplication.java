c500aef24172653fb1d886f783c46c9899452390
Renato <renato.rjps@gmail.com>
2019-07-10 14:10:59 -0300

    Initial backend commit

---
+++ backend/src/main/java/br/com/rjps/pixeon/PixeonApplication.java
@@ -0,0 +1,13 @@
+package br.com.rjps.pixeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixeonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/rjps/pixeon/PixeonApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.rjps.pixeon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PixeonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
